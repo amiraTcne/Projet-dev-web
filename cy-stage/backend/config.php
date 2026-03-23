9e1925ddc37008abf2d587b64968d2b0b3f5c836
aminaattaf <aminaattaf31@gmail.com>
2026-03-23 16:19:02 +0100

    Update config.php

--- cy-stage/backend/config.php
+++ cy-stage/backend/config.php
@@ -1,1 +1,16 @@
+<?php
+    $host = 'localhost';
+    $user = 'use'; // remplacer par votre user
+    $motdepass ='motDePasse'; // remplacer par votre mot de passe
+    $db = 'cyStage';
 
+    $connect = mysqli_connect($host,$user,$motdepass,$db);
+
+    if(!$connect){
+
+        die("connection mauvaise:" . mysqli_connect_error());
+    }
+
+    mysqli_close($connect);
+
+    ?>

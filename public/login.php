131f6abe3aa18e0d46571dc792eb40bf68e957a2
Ambre <122352315+ambre66160@users.noreply.github.com>
2026-03-24 12:22:51 +0100

    modification de login.php

--- public/login.php
+++ public/login.php
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Connexion</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="logo_connexion" src="/assets/img/logo.png">
+    <h2>Stage</h2>
+    <h1>Connexion</h1>
+    <form action="verifierConnexion.php" method="POST">
+        <label>Login :</label><br>
+        <input type="text" id="login" name="login" required><br>
 
+        <label>Mot de passe :</label><br>
+        <input type="password" id="mdp" name="mdp" required><br><br>
+
+        <input type="submit" value="Connexion">
+    </form>
+    <a href="#">Mot de passe oublié ? </a>
+   
+</body>
+</html>
